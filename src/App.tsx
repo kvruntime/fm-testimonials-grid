6129128cfcb0497c3f53dd1737b00fc690c21a81
kvruntime <86605865+kvruntime@users.noreply.github.com>
2024-09-28 23:26:50 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi velit, labore magnam ipsum dolore quidem, iste possimus porro similique vero facilis officiis esse corrupti neque voluptatibus et mollitia delectus id!
+      </p>
+    </div>
+  )
+}
+
+export default App
